fb231bec5e9e24d8a34f84e8cc77709f5a7c0fa1
Gabe571 <gabriel.castro1028@gmail.com>
2021-01-20 11:49:45 -0800

    UserController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UserController < ApplicationController
+
+    def create
+        @user = user.create(user_params)
+        if @user.valid?
+            token = encode_token{user_id: @user.id}
+            render json: {error:'Gamer Recognized!'}
+        else
+            render json: {error: 'Failed to create Gamer'}
+        end
+    end
+
+    def login
+        @user =user.find_by(username:params[:username])
+        if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user:UserSerializer.new(@user), token:token}
+        end
+      else
+        render json: {error: "wrong username and/or password"}
+      end
+    end
+ 
+    def index
+        @user = User.all
+        render json: @user
+    end
+
+    def persist
+        token= encode_token({user_id: @user.id})
+        render json: {user:UserSerializer.new(@user), token:token}
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :games, :through :reviews
+    has_many :games, through: :reviews
     has_secure_password
 end
