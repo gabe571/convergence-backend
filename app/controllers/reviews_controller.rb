9af7ee87bf8ac9bb8d3bf1a76f4b5692cb3da4c0
Gabe571 <gabriel.castro1028@gmail.com>
2021-02-15 15:39:39 -0800

    working thru reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
     # before_action :authorized, only:[:create]
+    before_action :authenticate_with_http_digest, only: [:new, :create]
 
     def index
         reviews = Review.all
@@ -12,7 +13,7 @@ class ReviewsController < ApplicationController
     end
 
     def create
-        game = game.find_or_create_by(name: params[:review][:game])
+        game = Game.find_or_create_by(name: params[:review][:game_name])
         review = Review.new(review_params)
         review.game_id = game.id
         review.user_id = @user.id
@@ -34,6 +35,6 @@ class ReviewsController < ApplicationController
     end
     private
     def review_params
-        params.require(:review).permit(:user_id, :game_id, :user_username, :game_name, :reviewed_game, :rating)
+        params.require(:review).permit(:user_id, :game_id, :user_username, :reviewed_game, :rating, :game_name)
     end
 end
